46725d3df643d6785c83d8da3ef70bda19693b4d
wangxijin <188512936@qq.com>
2018-08-22 10:51:08 +0800

    add meizhuo and toutiao spiders

---
+++ meizhuo_spider.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+ 
+'''
+@Author  :   Anuo.
+ 
+@License :   (C) Copyright 2018, Anuo's Studio
+ 
+@Contact :   188512936@qq.com
+ 
+@Software:   VS2017
+ 
+@File    :   meizhuo_spider.py
+ 
+@Time    :   Aug 22,2018
+ 
+@Desc    :   美桌网美女图片爬虫。
+ 
+'''
+
+import os
+import sys
+import requests
+from bs4 import BeautifulSoup
+from multiprocessing.pool import Pool
+
+
+def main():
+    '''主流程：提取图集，然后启动多线程对每个图集的图进行下载'''
+
+    url = 'http://www.win4000.com/meitu.html'
+    resp = requests.get(url)
+    if resp.status_code  == 200:
+        soup = BeautifulSoup(resp.text,'lxml')
+        lis = soup.find_all(name='div',attrs={'class':"list_cont list_cont2 w1180"})
+        image_dict = {}
+
+        for node in lis:
+            lis2 = node.find_all(name = 'li')
+            for node2 in lis2:
+                name = node2.p.string
+                url2 = node2.a.attrs['href']
+                image_dict[name] = url2
+
+        if image_dict:
+            deal_image_dict(image_dict)
+
+
+def deal_image_dict(image_dict):
+    '''多线程下载每一个图集'''
+
+    sys.setrecursionlimit(10000) #例如这里设置为一百万
+    pool = Pool()
+    pool.map(deal_each,image_dict.items())
+    pool.close()
+    pool.join()
+
+
+def deal_each(item):
+    '''下载每一个图集'''
+
+    name = item[0]
+    url = item[1]
+
+    if not os.path.exists(name):
+        os.mkdir(name)
+
+    #替换.html      如    html  - >  _1.html
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.text,'lxml')
+        number = (int)(soup.select('body > div.main > div > div.pic_main > div > div.Bigimg > div.ptitle > em')[0].text)
+
+        for i in range(0,number + 1):
+            url2 = url
+            if i != 0:
+                new_str = '_' + str(i) + '.html'
+                url2 = url.replace('.html',new_str)
+
+            resp = requests.get(url2)
+            if resp.status_code == 200:
+                soup = BeautifulSoup(resp.text,'lxml')
+                image_url = soup.select('#pic-meinv > a > img')[0].attrs['data-original']
+
+                resp = requests.get(image_url)
+                if resp.status_code == 200:
+                    file_path = '{0}/{1}.{2}'.format(name,i,'jpg')
+
+                    if not os.path.exists(file_path):
+                        with open(file_path,'wb') as f:
+                            f.write(resp.content)
+                            print(file_path + '\n')
+
+        
+if __name__ == "__main__":
+    main()
---
+++ toutiao_spider.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+ 
+'''
+@Author  :   Anuo.
+ 
+@License :   (C) Copyright 2018, Anuo's Studio
+ 
+@Contact :   188512936@qq.com
+ 
+@Software:   VS2017
+ 
+@File    :   toutiao_spider.py
+ 
+@Time    :   Aug 21,2018
+ 
+@Desc    :   头条街拍图片爬虫。
+ 
+'''
+
+import os
+import requests
+from hashlib import md5
+from urllib.parse import urlencode
+from multiprocessing.pool import Pool
+
+def get_page(offset):
+    '''爬取网页'''
+
+    parms = {
+        'offset':offset,
+        'format':'json',
+        'keyword':'街拍',
+        'autoload':'true',
+        'count':'20',
+        'cur_tab':'1',
+        'from':'search_tab'
+        }
+    url = 'https://www.toutiao.com/search_content/?' + urlencode(parms)
+    try:
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            return resp.json()
+    except requests.ConnectionError:
+        return None
+
+def get_image(json):
+    '''爬取图片'''
+
+    if json:
+        items =  json.get('data')
+        if items:
+            for item in items:
+                title = item.get('title')
+                images = item.get('image_list')
+                if images:
+                    for image in images:
+                        yield {
+                            'image':'http:' + image.get('url'),
+                            'title':title
+                            }
+
+def save_image(item):
+    '''保存图片'''
+
+    if not os.path.exists(item.get('title')):
+        os.mkdir(item.get('title'))
+    try:
+        resp = requests.get(item.get('image'))
+        if resp.status_code == 200:
+            file_path = '{0}/{1}.{2}'.format(item.get('title'),md5(resp.content).hexdigest(),'jpg')
+            if not os.path.exists(file_path):
+                with open(file_path,'wb') as f:
+                    f.write(resp.content)
+            else:
+                print('Already Downloaded',file_path)
+    except requests.ConnectionError:
+        print('File to save image.')
+
+def main(offset):
+    '''主流程'''
+
+    json = get_page(offset)
+    for item in get_image(json):
+        print(item)
+        save_image(item)
+
+GROUP_START = 1
+GROUP_END = 20
+
+if __name__ == '__main__':
+    pool = Pool()
+    groups = ([x * 20 for x in range(GROUP_START,GROUP_END + 1)])
+    pool.map(main,groups)
+    pool.close()
+    pool.join()
